7502b4f62a69ecef0149e7effd30a0b379b068d9
OKhanzhyn <dev.khan.world@gmail.com>
2024-01-25 02:11:16 +0200

    HW10_02_01

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,23 +3,40 @@ import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
+// const fulfilledTick = document.querySelector('input[value="fulfilled"]');
+// const rejectedTick = document.querySelector('input[value="rejected"]');
+const form = document.querySelector(".form");
 const notifBtn = document.querySelector('button[type="submit"]');
-const fulfilledTick = document.querySelector('input[value="fulfilled"]');
-const rejectedTick = document.querySelector('input[value="rejected"]');
 const delayInput = document.querySelector('input[type="number"]');
-
 notifBtn.addEventListener('click', handleCreateNotif);
+form.addEventListener('submit', onSubmitForm);
+
+// Краще брати значення цього елемента форми через elements і в сеттаймауті порівнювати його з конкретним значенням, якщо воно дорівнює "fulfilled" - викликаєте resolve і тільки передаєте туди значення затримки. якщо значення дорівнює "rejected" - ви викликаєте reject і знову ж таки тільки значення передаєте.
+// А вже ізітоаст з конкретними повідомленнями ви маєте викликати при обробці промісов, в then/catch. 
+
+function onSubmitForm(event) {
+event.preventDefault();
+}
+ //Вибираємо усі елементи форми
+ const formElements = form.elements;
+setTimeout(() => {
+if () {
+    
+}
+})
+
 
 function handleCreateNotif() 
 {
-    const delay = delayInput.toString.value;
+    const delay = delayInput.value;
 console.log(delay);
+
     const promises = new Promise((resolve, reject) => 
     {
       const isPromiseFulfilled = ('');
 
        if (isPromiseFulfilled) 
-       { fulfilledTick.onchange
+       { fulfilledTick.click;
           resolve("delay");
           iziToast.success({
             title: 'OK',

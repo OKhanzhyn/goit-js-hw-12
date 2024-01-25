25b3b1566c68a101b4f26ded2aede571eb1e3079
OKhanzhyn <dev.khan.world@gmail.com>
2024-01-26 01:12:31 +0200

    HW10_02_04

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,76 +1,40 @@
-// Описаний у документації
 import iziToast from "izitoast";
-// Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
-const fulfilledTick = document.querySelector('input[value="fulfilled"]');
-const rejectedTick = document.querySelector('input[value="rejected"]');
 const form = document.querySelector(".form");
 const notifBtn = document.querySelector('button[type="submit"]');
 const delayInput = document.querySelector('input[type="number"]');
-// notifBtn.addEventListener('click', handleCreateNotif);
+
 form.addEventListener('submit', onSubmitForm);
 
-const formElements = form.elements;
+function onSubmitForm(event) {
+event.preventDefault();
+
 const delay = delayInput.value;
 console.log(delay);
-
-function onSubmitForm(event) {
-// event.preventDefault();
-return new Promise((resolve, reject) => 
+const promise = new Promise((resolve, reject) => 
 {
-
-
-setTimeout(() => {
-if (fulfilledTick.onchenge) {
-resolve(console.log(`✅ Fulfilled promise in ${delay}ms`));}
-else {
-reject(console.log(`❌ Rejected promise in ${delay}ms`));}
-}, delay);
-});
-};
-
-onSubmitForm().then((result) => {
+  setTimeout(() => {
+    const radioBtn = form.state.value; 
+  if (radioBtn === 'fulfilled') {
+   resolve(delay);}
+  if (radioBtn === "rejected") {
+    reject(delay);}
+    console.log(radioBtn);
+//   else {
+//    return}
+  }, delay);
+  });
+   promise.then((result) => {
     iziToast.success({
         title: 'OK',
-        message: 'Fulfilled promise in ${delay}ms',
+        message: `Fulfilled promise in ${delay}ms`,
       });
-})
-.catch((error) => {
+  })
+   .catch((error) => {
     iziToast.error({
         title: '',
-        message: 'Rejected promise in ${delay}ms',
+        message: `Rejected promise in ${delay}ms`,
     });
 });
-
-
-
-// function handleCreateNotif() 
-// {
-//     const delay = delayInput.value;
-// console.log(delay);
-
-//     const promises = new Promise((resolve, reject) => 
-//     {
-//       const isPromiseFulfilled = ('');
-
-//        if (isPromiseFulfilled) 
-//        { fulfilledTick.click;
-//           resolve("delay");
-//           iziToast.success({
-//             title: 'OK',
-//             message: 'Fulfilled promise in ${delay}ms',
-//           });
-//        }
-//         else 
-//         {  rejectedTick.onchange;
-//             reject('delay');
-//             iziToast.error({
-//                 title: '',
-//                 message: 'Rejected promise in ${delay}ms',
-//             });
-//         }       
-//     })
-// };
-// Краще брати значення цього елемента форми через elements і в сеттаймауті порівнювати його з конкретним значенням, якщо воно дорівнює "fulfilled" - викликаєте resolve і тільки передаєте туди значення затримки. якщо значення дорівнює "rejected" - ви викликаєте reject і знову ж таки тільки значення передаєте.
-// А вже ізітоаст з конкретними повідомленнями ви маєте викликати при обробці промісов, в then/catch. 
+};

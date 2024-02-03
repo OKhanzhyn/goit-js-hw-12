bcd143ff092f57b7eda0da0d9983a30ec22b9dcc
OKhanzhyn <dev.khan.world@gmail.com>
2024-02-03 17:01:50 +0200

    HW12_03_loadMoreBtn

--- src/js/photoApi.js
+++ src/js/photoApi.js
@@ -3,16 +3,16 @@ const API_KEY = '42048563-a2c01b7234988bf152885bd8d';
 
 // https://pixabay.com/api/?key=42048563-a2c01b7234988bf152885bd8d&q=photoWrd&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=15
 
-function searchPhotoByWrd(photoWrd) {
+function searchPhotoByWrd(photoWrd, page = 1) {
     return axios
-    .get(`${BASE_URL}`, {
+    .get(`${BASE_URL}/`, {
         params: {
         key: API_KEY,
         q: photoWrd,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-        page: 1,
+        page,
         per_page: 15,
         },
     }).then(({ data }) => data);
--- src/main.js
+++ src/main.js
@@ -9,22 +9,23 @@ import { searchPhotoByWrd } from "./js/photoApi.js";
 
 const refs = {
     form: document.getElementById('form'),
-    resultContainer: document.getElementById('resultContainer'),
+    // resultContainer: document.getElementById('resultContainer'),
     loaderContainer: document.querySelector('.loader'),
     gallery: document.querySelector('.gallery'),
     loadMoreBtn: document.querySelector('.load-btn'),
+    preloader: document.querySelector('.loader-more'),
 };
 const hiddenClass = 'is-hidden';
-// let page = 1;
-// let photoWrd = '';
+let page = 1;
+let photoWrd = '';
 
 refs.form.addEventListener('submit', handleSearch);
 
 async function handleSearch(event) {
     event.preventDefault();
-    // page = 1;
+    page = 1;
     const form = event.currentTarget;
-    const photoWrd = form.elements.photoWrd.value.trim();
+    photoWrd = form.elements.photoWrd.value.trim();
          
     if(!photoWrd) {
         iziToast.error({
@@ -81,43 +82,56 @@ try {
       form.reset();
     }
 }
-// async function handleLoadMore () {
-// page += 1;
-// try {
-//     await searchPhotoByWrd(photoWrd, page).then((data) => {   
-//         console.log(data);
-       
-    //    if (data.hits.length === 0) {
-    //     iziToast.error({
-    //         message: "Sorry, there are no images matching your search query. Please try again!",
-    //         theme: 'dark',
-    //         messageSize: '16px',
-    //         messageColor: 'white',
-    //         backgroundColor: '#ef4040',
-    //         position: 'topRight',
-    //         maxWidth: '390px',
-    //         timeout: 5000,
-    //     });   return;
-    //   }
-       
-    // let markup = "";    
-    //   data.hits.forEach((i) => {        
-    //    console.log(i);
-    //    markup += createPhotoCardMarkup(i);
-    //    });             
-    // refs.gallery.innerHTML = markup;
-    // lightbox .refresh();  
-    // if (data.hits.length > 0) {
-    //     refs.loadMoreBtn.classList.remove(hiddenClass); 
-    //     refs.loadMoreBtn.addEventListener('click', handleLoadMore);
-    // } else {
-    //     refs.loadMoreBtn.classList.add(hiddenClass); 
-    // }       
-//     })} catch (err) {
-//         console.log(err);
-//     }
 
-// }
+async function handleLoadMore () {
+page += 1;
+refs.preloader.classList.remove(hiddenClass);
+refs.loadMoreBtn.disabled = true;
+
+try {
+    await searchPhotoByWrd(photoWrd, page).then((data) => {   
+        console.log(data);
+       
+       if (data.hits.length === 0) {
+        iziToast.error({
+            message: "Sorry, there are no images matching your search query. Please try again!",
+            theme: 'dark',
+            messageSize: '16px',
+            messageColor: 'white',
+            backgroundColor: '#ef4040',
+            position: 'topRight',
+            maxWidth: '390px',
+            timeout: 5000,
+        });   return;
+      }
+    
+    let markup = "";    
+      data.hits.forEach((i) => {        
+       console.log(i);
+       markup += createPhotoCardMarkup(i);
+       });             
+       refs.gallery.insertAdjacentHTML('beforeend', markup );
+    lightbox .refresh();  
+    if (data.hits.length > 0) {
+        refs.loadMoreBtn.classList.remove(hiddenClass); 
+        refs.loadMoreBtn.addEventListener('click', handleLoadMore);
+    } else {
+        refs.loadMoreBtn.classList.add(hiddenClass); 
+    }       
+    // refs.preloader.classList.add(hiddenClass);
+    //     refs.loadMoreBtn.disabled = false;
+    })} catch (err) {
+        console.log(err);
+    } 
+    finally {
+        refs.preloader.classList.add(hiddenClass);
+        refs.loadMoreBtn.disabled = false;
+    //     refs.preloader.style.display = 'none';
+    //     refs.loadMoreBtn.disabled = false;
+    }
+
+}
+
 const lightbox = new SimpleLightbox('.gallery a', {
     caption: true,
     captionDelay: 250,
